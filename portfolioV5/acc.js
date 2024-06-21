f0e074d98d7041d0fd37c37db6b2ed5871272960
Tometu-cyber <146548356+Tometu-cyber@users.noreply.github.com>
2024-06-21 13:39:44 +0200

    MAJ taille image + upgrade

---
+++ portfolioV5/acc.js
@@ -0,0 +1,115 @@
+const menuBtn = document.querySelector(".menu-btn");
+const wrapper = document.querySelector(".navbar");
+
+menuBtn.addEventListener("click", function () {
+  menuBtn.querySelector("i").classList.toggle("fa-xmark");
+  wrapper.classList.toggle("open");
+});
+
+document.querySelectorAll('.link').forEach(function(link) {
+  link.addEventListener('click', function() {
+    menuBtn.querySelector('i').classList.remove('fa-xmark');
+    wrapper.classList.remove('open');
+  });
+});
+
+// Récupérer les boîtes par leur ID
+var box1 = document.getElementById("box1");
+var box2 = document.getElementById("box2");
+var box3 = document.getElementById("box3");
+var box4 = document.getElementById("box4");
+var box5 = document.getElementById("box5");
+var box6 = document.getElementById("box6");
+
+// Récupérer les boîtes modales
+var py = document.getElementById("py");
+var c = document.getElementById("c");
+var html = document.getElementById("html");
+var thales = document.getElementById("thales");
+var routeur = document.getElementById("routeur");
+var reseaux = document.getElementById("reseaux");
+
+// Récupérer les boutons de fermeture
+var spanPy = py.getElementsByClassName("close")[0];
+var spanC = c.getElementsByClassName("close")[0];
+var spanHtml = html.getElementsByClassName("close")[0];
+var spanThales = thales.getElementsByClassName("close")[0];
+var spanRouteur = routeur.getElementsByClassName("close")[0];
+var spanReseaux = reseaux.getElementsByClassName("close")[0];
+
+// Ajouter des gestionnaires d'événements de clic pour chaque boîte
+box1.addEventListener("click", function() {
+  py.style.display = "block";
+});
+
+box2.addEventListener("click", function() {
+  c.style.display = "block";
+});
+
+box3.addEventListener("click", function() {
+  html.style.display = "block";
+});
+
+box4.addEventListener("click", function() {
+  thales.style.display = "block";
+});
+
+box5.addEventListener("click", function() {
+  routeur.style.display = "block";
+});
+
+box6.addEventListener("click", function() {
+  reseaux.style.display = "block";
+});
+
+// Ajouter des gestionnaires d'événements de clic pour chaque bouton de fermeture
+spanPy.addEventListener("click", function() {
+  py.style.display = "none";
+});
+
+spanC.addEventListener("click", function() {
+  c.style.display = "none";
+});
+
+spanHtml.addEventListener("click", function() {
+  html.style.display = "none";
+});
+
+spanThales.addEventListener("click", function() {
+  thales.style.display = "none";
+});
+
+spanRouteur.addEventListener("click", function() {
+  routeur.style.display = "none";
+});
+
+spanReseaux.addEventListener("click", function() {
+  reseaux.style.display = "none";
+});
+
+// Fermer la boîte modale si l'utilisateur clique en dehors de celle-ci
+window.addEventListener("click", function(event) {
+  if (event.target == py) {
+    py.style.display = "none";
+  }
+
+  if (event.target == c) {
+    c.style.display = "none";
+  }
+
+  if (event.target == html) {
+    html.style.display = "none";
+  }
+
+  if (event.target == thales) {
+    thales.style.display = "none";
+  }
+
+  if (event.target == routeur) {
+    routeur.style.display = "none";
+  }
+
+  if (event.target == reseaux) {
+    reseaux.style.display = "none";
+  }
+});
